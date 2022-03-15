41ff840e0fcd7d6df64a15bfa6fb58ec02eac6cb
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-03-15 19:03:53 +0100

    Properly fix the loading screen with the default option Vanilla literally waits 2 seconds before being able to close the screen, no matter what has been loaded or sent. This fixes that in a quick and easy way by ignoring their "skipped tick" once the default spawn position has been received (after a server side teleport).

--- src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/ReceivingLevelScreenMixin.java
+++ src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/ReceivingLevelScreenMixin.java
@@ -2,24 +2,25 @@ package eu.kennytv.forcecloseloadingscreen.mixin;
 
 import net.minecraft.client.gui.screens.ReceivingLevelScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ReceivingLevelScreen.class)
 public abstract class ReceivingLevelScreenMixin extends Screen {
+
+    @Shadow
+    private boolean oneTickSkipped;
 
     protected ReceivingLevelScreenMixin(final Component component) {
         super(component);
     }
 
-    @Redirect(method = "tick",
-            at = @At(target = "Lnet/minecraft/client/renderer/LevelRenderer;isChunkCompiled(Lnet/minecraft/core/BlockPos;)Z", value = "INVOKE")
-    )
-    private boolean isChunkCompiled(final LevelRenderer instance, final BlockPos blockPos) {
-        return true;
+    @Inject(at = @At("HEAD"), method = "loadingPacketsReceived")
+    public void tick(final CallbackInfo ci) {
+        this.oneTickSkipped = true;
     }
 }
