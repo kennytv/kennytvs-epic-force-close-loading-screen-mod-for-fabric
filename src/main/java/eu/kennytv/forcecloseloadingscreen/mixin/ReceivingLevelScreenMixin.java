94f33b4335ff3b87d64e5a4896eb41e2628bf58d
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-05-20 15:00:05 +0200

    Set required version to >=1.18.2

--- src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/ReceivingLevelScreenMixin.java
+++ src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/ReceivingLevelScreenMixin.java
@@ -1,8 +1,6 @@
 package eu.kennytv.forcecloseloadingscreen.mixin;
 
 import net.minecraft.client.gui.screens.ReceivingLevelScreen;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,14 +8,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ReceivingLevelScreen.class)
-public abstract class ReceivingLevelScreenMixin extends Screen {
+public abstract class ReceivingLevelScreenMixin {
 
     @Shadow
     private boolean oneTickSkipped;
-
-    protected ReceivingLevelScreenMixin(final Component component) {
-        super(component);
-    }
 
     @Inject(at = @At("HEAD"), method = "loadingPacketsReceived")
     public void tick(final CallbackInfo ci) {
