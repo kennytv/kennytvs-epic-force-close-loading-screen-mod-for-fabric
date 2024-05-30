25bcaaf6fb10db14ae2c399636a26bda66f9a2b9
David Luedtke <david@ldtke.de>
2024-05-30 12:27:19 +0200

    fix codestyle

--- src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/MouseHandlerMixin.java
+++ src/main/java/eu/kennytv/forcecloseloadingscreen/mixin/MouseHandlerMixin.java
@@ -3,7 +3,6 @@ package eu.kennytv.forcecloseloadingscreen.mixin;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,12 +22,8 @@ public class MouseHandlerMixin {
 
     @Inject(method = "grabMouse", at = @At(value = "HEAD", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"))
     public void fixSprint(CallbackInfo ci) {
-        if (this.minecraft.isWindowActive()) {
-            if (this.mouseGrabbed) {
-                if (!Minecraft.ON_OSX) {
-                    KeyMapping.setAll();
-                }
-            }
+        if (this.minecraft.isWindowActive() && this.mouseGrabbed && !Minecraft.ON_OSX) {
+            KeyMapping.setAll();
         }
     }
 }
